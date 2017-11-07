62f35bb54570fe5432343662309f8be7a0d2dfe2
sturgilltyffani <sturgilltyffani@gmail.com>
2017-11-07 11:59:09 -0600

    need to convert js to ts

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as bodyparser from 'body-parser';
+import { generate } from 'shortid';
+
+//make a get request for all chirps
+//make a get request for one chirp
+//make a post request to add chirp
+//make a delete request to remove a message from the data
+//make a put request to update a chirp
+//use at least one middleware
+//use modules
